ecaf3605c74d43b825a2d0003ede293181879ab7
nginisty <nginisty.pro@gmail.com>
2022-12-08 07:55:56 +0100

    form and agenda prepared

--- js/contact.js
+++ js/contact.js
@@ -4,17 +4,34 @@ var intervenantID = window.location.href.split('#')[1];
 
 function LoadAgendaByID(_intervenantID) { }
 
-function UpdateAgendaByID(_intervenantID) { }
+function UpdateAgendaByID(_intervenantID) {
+    document.getElementById("agenda").innerHTML = "agenda de " + intervenantID;
+}
 
 function UpdateFormByID(_intervenantID) {
-    var z = document.getElementById("intervenants").value = intervenantID;
+    document.getElementById("intervenants").value = intervenantID;
 }
 
 function UpdateIntervenant(_intervenantID) {
+    document.location.href = window.location.href.split('#')[0] + "#" + _intervenantID;
+    intervenantID = _intervenantID;
     UpdateAgendaByID(_intervenantID);
     UpdateFormByID(_intervenantID);
+    // console.log("update to " + document.getElementById("intervenants").value);
 }
 
-// Load init page
+function PageInit() {
+    if (intervenantID == "" || intervenantID == null) {
+        intervenantID = document.getElementById("intervenants").value;
+    }
 
-UpdateIntervenant(intervenantID);
+    UpdateIntervenant(intervenantID);
+}
+
+
+// ---------------------------------- //
+// -------------- INIT -------------- //
+// ---------------------------------- //
+
+PageInit();
+document.addEventListener("click", function () { UpdateIntervenant(document.getElementById("intervenants").value) });
